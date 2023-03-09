6310b654db1b69173b4beb71e8f2805087827067
ang <57077187+constSobri@users.noreply.github.com>
2023-03-08 20:20:11 -0600

    comienzo

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    $(".btn").click(()=> {
+        console.log('Hola, estoy utilizando jQuery')
+    })
+  });
+
+
+function alerta() {
+    alert('click en el botón')
+}
